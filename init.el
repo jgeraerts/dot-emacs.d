e867cae33a1222bc2fd896f0d6dabcf08f985434
Jo Geraerts <jo@umask.net>
2016-09-02 09:25:31 +0200

    remove duplicate block

--- init.el
+++ init.el
@@ -173,12 +173,6 @@
     ;; This choice of keybinding leaves cider-macroexpand-1 unbound
     (cljr-add-keybindings-with-prefix "C-c C-m"))
 
-(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)(defun my-clojure-mode-hook ()
-    (clj-refactor-mode 1)
-    (yas-minor-mode 1) ; for adding require/use/import statements
-    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
-    (cljr-add-keybindings-with-prefix "C-c C-m"))
-
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
