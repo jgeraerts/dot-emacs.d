a11beb2949edb0d0b1fc7d658b7b8135f14ad678
Jo Geraerts <jo@umask.net>
2016-12-27 07:45:30 +0100

    cleanup packages and delay cider startup

--- init.el
+++ init.el
@@ -1,10 +1,27 @@
 ;;; init --- Emacs Initialization File
 ;;; Commentary:
-;;;    make flycheck happy
+;;;
+
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
+(setq settings-dir
+      (expand-file-name "settings" user-emacs-directory))
+
+;; Add external projects to load path
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)    
+    (add-to-list 'load-path project)))
 
 
-;;; This caused emacs freeze when my laptop is disconnected from the DC
+(prefer-coding-system 'utf-8-unix)
+
+;(package-initialize)
+
 (setq w32-get-true-file-attributes nil)
+
+
+
 
 ;;; Code:
 ;; Turn off mouse interface early in startup to avoid momentary display
@@ -15,21 +32,11 @@
 ;; No splash screen please ... jeez
 (setq inhibit-startup-message t)
 
-;; Set path to dependencies
-(setq site-lisp-dir
-      (expand-file-name "site-lisp" user-emacs-directory))
-
-(setq settings-dir
-      (expand-file-name "settings" user-emacs-directory))
 
 ;; Set up load path
 (add-to-list 'load-path settings-dir)
 (add-to-list 'load-path site-lisp-dir)
 
-;; Add external projects to load path
-(dolist (project (directory-files site-lisp-dir t "\\w+"))
-  (when (file-directory-p project)    
-    (add-to-list 'load-path project)))
 
 ;; Save point position between sessions
 (require 'saveplace)
@@ -41,39 +48,47 @@
 
 (defun init--install-packages ()
   (packages-install
-    '(better-defaults
-      paredit
-      idle-highlight-mode
-      ido-ubiquitous
-      flx-ido
-      ido-vertical-mode
-      find-file-in-project
-      smex
-                                        ;scpaste
-      company
-      cider
-      clojure-mode
-      clojure-mode-extra-font-locking
-      clj-refactor
-      rainbow-delimiters
-      graphviz-dot-mode
-      magit
-      slamhound
-      projectile
-      zenburn-theme
-      fill-column-indicator
-      expand-region
-      powerline
-      smart-mode-line
-      discover
-      discover-my-major
-      which-key
-      markdown-mode
-      flycheck
-      company-jedi
-      smooth-scrolling
-      undo-tree
-      whitespace-cleanup-mode)))
+   '(
+     better-defaults
+     cider
+     clj-refactor
+     clojure-mode
+     clojure-mode-extra-font-locking
+     company
+     company-jedi
+     discover
+     discover-my-major
+     edn
+     expand-region
+     fill-column-indicator
+     find-file-in-project
+     flx-ido
+     flycheck
+     graphviz-dot-mode
+     hydra
+     idle-highlight-mode
+     ido-ubiquitous
+     ido-vertical-mode
+     inflections
+     magit
+     markdown-mode
+     multiple-cursors
+     paredit
+     powerline
+     projectile
+     puppet-mode
+     rainbow-delimiters
+     slamhound
+     smart-mode-line
+     smex
+     smooth-scrolling
+     undo-tree
+     which-key
+     whitespace-cleanup-mode
+     yaml-mode
+     yasnippet
+     zenburn-theme
+     )))
 
 (condition-case nil
     (init--install-packages)
@@ -82,11 +97,10 @@
    (init--install-packages)))
 
 (require 'sane-defaults)
-
 (require 'powerline)
 (require 'fill-column-indicator) ;; line indicating some edge column
 (require 'rainbow-delimiters)
-(require 'cider)
+;(require 'cider)
 (require 'which-key)
 (require 'flx-ido)
 (require 'company)
@@ -119,7 +133,6 @@
 ; up-down works better in vertical mode
 (setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right)
 (load-theme 'zenburn t)
-(prefer-coding-system 'utf-8-unix)
 (projectile-global-mode)
 ;(global-linum-mode t)               ; Always show line numbers on left
 (global-fci-mode 1)
@@ -131,10 +144,6 @@
 
 ;; Configure nrepl.el
 ;(setq nrepl-hide-special-buffers t)
-(setq cider-repl-popup-stacktraces-in-repl t)
-(setq nrepl-popup-on-error nil) ; Don't popup new buffer for errors (show in nrepl buffer)
-(setq cider-repl-history-file "~/.emacs.d/nrepl-history")
-(setq nrepl-log-messages t)
 
 (setq TeX-engine 'xetex)
 (setq ido-enable-prefix nil
@@ -167,10 +176,16 @@
 
 
 (defun my-clojure-mode-hook ()
-    (clj-refactor-mode 1)
-    (yas-minor-mode 1) ; for adding require/use/import statements
-    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
-    (cljr-add-keybindings-with-prefix "C-c C-m"))
+  (require 'cider)
+  (define-key clojure-mode-map (kbd "C-c M-y") 'cider-namespace-refresh)
+  (setq cider-repl-popup-stacktraces-in-repl t)
+  (setq nrepl-popup-on-error nil) ; Don't popup new buffer for errors (show in nrepl buffer)
+  (setq cider-repl-history-file "~/.emacs.d/nrepl-history")
+  (setq nrepl-log-messages t)
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1) ; for adding require/use/import statements
+  ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
 
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
@@ -194,7 +209,7 @@
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
-(define-key clojure-mode-map (kbd "C-c M-y") 'cider-namespace-refresh)
+
 
 ;;; whitespace setup
 (setq whitespace-style '(trailing lines space-before-tab
